73308b5202f262c99d8476edbcc0bd2f035a2e3a
Nabil Siddiqui <47227453+nabeelsid@users.noreply.github.com>
2020-09-05 13:26:05 -0400

    Add fade, unfade and update weather functions
    - Fade in DOM objects such as div and text input as they are shown using opacity style properties
    - Weather can now be updated every 1 minute. Will support user defined update intervals in future update

--- src/index.js
+++ src/index.js
@@ -2,26 +2,69 @@ const electron = require("electron");
 const path = require("path");
 const BrowserWindow = electron.remote.BrowserWindow;
 let list = document.querySelectorAll("#city");
+let inputfield = document.getElementById("cityInput");
+let initialText = document.getElementById("first-start");
 let cityList = [];
 let curtmp;
+let skyCondition;
+let cityCount = 0;
+let firstRun = true;
+
+//function to make first letter uppercase
+function stringNormalize(string) 
+{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function fade(element) {
+  var op = 1;  // initial opacity
+  var timer = setInterval(function () {
+      if (op <= 0.1){
+          clearInterval(timer);
+          element.style.display = 'none';
+      }
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op -= op * 0.1;
+  }, 50);
+}
+
+function unfade(element) {
+  var op = 0.1;  // initial opacity
+  element.style.display = 'block';
+  var timer = setInterval(function () {
+      if (op >= 1){
+          clearInterval(timer);
+      }
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op += op * 0.1;
+  }, 10);
+}
+unfade(initialText);
 
 //function to add a new city to list
 function addCity(cityName, position) {
   cityList.push(cityName);
   const div = document.createElement("div");
   div.className = "box";
+  div.style.opacity = 0.1
   div.innerHTML =
     `<h1 id="city">` +
-    cityName +
+    stringNormalize(cityName) + `<img class="graphic" id="skyCondition` +position + `" src="../assets/images/question.svg" alt=""></img>` + 
     `<span class="currentTemp" id="currentTemp` +
     position +
-    `">Loading..</span></h1>
+    `"> </span></h1>
     <span id="min` +
     position +
-    `" class="min">Min: 19 C</span> <span id="max` +
+    `" class="min">Min: </span> <span id="max` +
     position +
-    `" class="max">Max: 26 C</span>`;
+    `" class="max">Max: </span>`;
   document.body.appendChild(div);
+  
+  unfade(div);
+  cityCount++;
+  console.log("im called!")
 }
 
 //iterate over each one in the list and extract the city name
@@ -32,14 +75,8 @@ for (i = 0; i < list.length; i++) {
   console.log(stringSplit[0]);
 }
 
-addCity("Mississauga", 0);
-addCity("Toronto", 1);
-addCity("Dubai", 2);
-addCity("Mumbai", 3);
-addCity("Los Angeles", 4);
-
 //getJSON function
-var getJSON = function (url, callback) {
+function getJSON(url, callback) {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", url, true);
   xhr.responseType = "json";
@@ -50,7 +87,7 @@ var getJSON = function (url, callback) {
     } else {
       callback(status, xhr.response);
     }
-  };
+  }
   xhr.send();
 };
 
@@ -66,60 +103,68 @@ function getData(cityName, position) {
     if (err !== null) {
       alert("Something went wrong: " + err);
     } else {
+      console.log(data.weather[0].main);
+      skyCondition = data.weather[0].main;
       curtmp = Math.round(data.main["temp"] - 273.0);
       mintmp = Math.round(data.main["temp_min"] - 273);
       maxtmp = Math.round(data.main["temp_max"] - 273);
-      document.getElementById("currentTemp" + position).innerText =
+      document.getElementById("currentTemp" + position).innerText = 
         curtmp + String.fromCharCode(176) + "c";
+
       document.getElementById("min" + position).innerText =
         "Min: " + mintmp + String.fromCharCode(176) + "c";
+
       document.getElementById("max" + position).innerText =
         "Max: " + maxtmp + String.fromCharCode(176) + "c";
+      if(skyCondition === "Clouds"){
+        document.getElementById("skyCondition" + position).src = "../assets/images/clouds.svg";
+      }
+      else if(skyCondition === "Haze" || skyCondition === "Fog" || skyCondition === "Smog"){
+        document.getElementById("skyCondition" + position).src = "../assets/images/haze-fog-smog.svg";
+      }
+      else if(skyCondition === "Partly Cloudy"){
+        document.getElementById("skyCondition" + position).src = "../assets/images/partly-cloudy.svg";
+      }
+      else if(skyCondition === "Clear"){
+        document.getElementById("skyCondition" + position).src = "../assets/images/clear.svg";
+      }
+      else if(skyCondition === "Rain"){
+        document.getElementById("skyCondition" + position).src = "../assets/images/snow.svg";
+      }
+      else if(skyCondition === "Snow"){
+        document.getElementById("skyCondition" + position).src = "../assets/images/snow.svg";
+      }
     }
   });
 }
 
-//go through all of the city list and call getData function on each on of them
-for (i = 0; i < cityList.length; i++) {
-  getData(cityList[i], i);
-}
-
 document.getElementById("addButton").addEventListener("click", function () {
+
   console.log("clicked");
-  var inputfield = document.getElementById("cityInput");
+  unfade(inputfield);
   inputfield.style.visibility='visible';
-  inputfield.addEventListener("keypress", function(e){
-
-    if(e.key === 'Enter'){
-
-      addCity(inputfield.value, 5);
-      getData(inputfield.value,5);
-      // console.log(inputfield.value);
-      inputfield.style.visibility='hidden';
-      // console.log("enter pressed");
-    }
-    
-  })
+  inputfield.focus();
 });
 
-// document.getElementById("addButton").addEventListener("click", function () {
-//   console.log("clicked");
-//   const modalPath = path.join("file://", __dirname, "add.html");
-//   let win = new BrowserWindow({
-//     frame: true,
-//     width: 400,
-//     height: 200,
-//     webPreferences: {
-//       enableRemoteModule: true,
-//     },
-//     webPreferences: {
-//       nodeIntegration: true,
-//     },
-//   });
-//   // win.webContents.openDevTools();
-//   win.on("close", function () {
-//     win = null;
-//   });
-//   win.loadURL(modalPath);
-//   win.show();
-// });
+inputfield.addEventListener("keydown", function(e){
+  if(e.key === 'Enter'){
+    if(firstRun){
+      initialText.remove();
+      firstRun = false;
+    }
+    addCity(inputfield.value, cityCount);
+    getData(inputfield.value, cityCount-1);
+    fade(inputfield);
+    // inputfield.style.visibility='hidden';
+    inputfield.value = '';
+  }
+});
+
+function updateWeather(){
+  // go through all of the city list and call getData function on each on of them
+  for (i = 0; i < cityList.length; i++) {
+    getData(cityList[i], i);
+  }
+}
+
+setTimeout(updateWeather, 60000);
